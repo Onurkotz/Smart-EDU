e0506c2298bee1c9187bf1bd0721ce11fd5d9bf7
onur koc <onurkottz@gmail.com>
2022-10-11 16:01:58 +0300

    course model, control and route

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+
 
 const app = express();
+// Connect database
+mongoose
+  .connect("mongodb://localhost/smartEduDB")
+  .then(() => {
+    console.log("Connected database.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 // Template Engine
 app.set("view engine", "ejs");
 
@@ -11,6 +23,8 @@ app.use(express.static("public"));
 
 // ROUTES
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+
 
 const port = 3000;
 app.listen(port, () => {
