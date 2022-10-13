6a938dd30cdd2fdc1e1e3c6a6fcc5af53694b2cf
onur koc <onurkottz@gmail.com>
2022-10-13 13:03:01 +0300

    single course pages

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
-
 
 const app = express();
 // Connect database
@@ -20,11 +20,12 @@ app.set("view engine", "ejs");
 
 // MIDDLEWARES
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ROUTES
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
-
 
 const port = 3000;
 app.listen(port, () => {
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const courseController = require("../controllers/courseController");
+const courseController = require("../controllers/courseContorller");
 
 const router = express.Router();
 
 router.route("/").post(courseController.createCourse);
-
+router.route("/").get(courseController.getAllCourses);
+router.route("/:id").get(courseController.getCourse);
 
 module.exports = router;
