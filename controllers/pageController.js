e62822d57232bdb0069241d5d23206fca7888d77
onur koc <onurkottz@gmail.com>
2022-10-14 11:20:29 +0300

    login

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -10,8 +10,14 @@ exports.getAbout = (req, res) => {
   });
 };
 
-exports.getRegisterPAge = (req, res) => {
+exports.getRegisterPage = (req, res) => {
   res.status(200).render("register", {
     page_name: "register",
+  });
+};
+
+exports.getLoginPage = (req, res) => {
+  res.status(200).render("login", {
+    page_name: "login",
   });
 };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.route("/").get(pageController.getHome);
 router.route("/about").get(pageController.getAbout);
-router.route("/register").get(pageController.getRegisterPAge);
+router.route("/register").get(pageController.getRegisterPage);
+router.route("/login").get(pageController.getLoginPage);
 
 module.exports = router;
