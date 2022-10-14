a818ca824a24a170e75ee2061573b27c438cd6ff
onur koc <onurkottz@gmail.com>
2022-10-14 11:58:23 +0300

    login2

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -6,6 +7,28 @@ exports.createUser = async (req, res) => {
     res.status(201).json({
       status: "Success",
       user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send("User logged in.");
+          }
+        });
+      }
     });
   } catch (error) {
     res.status(400).json({
