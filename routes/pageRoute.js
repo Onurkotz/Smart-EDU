a142247bfb4030398b13377aaf17094c820bbe7a
onur koc <onurkottz@gmail.com>
2022-10-17 13:44:07 +0300

    auth and redirect middlewares

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const pageController = require("../controllers/pageController");
+const redirectware = require('../middlewares/redirectware');
 
 const router = express.Router();
 
 router.route("/").get(pageController.getHome);
 router.route("/about").get(pageController.getAbout);
-router.route("/register").get(pageController.getRegisterPage);
-router.route("/login").get(pageController.getLoginPage);
+router.route("/register").get(redirectware, pageController.getRegisterPage);
+router.route("/login").get(redirectware, pageController.getLoginPage);
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const authware = require('../middlewares/authware');
 
 const router = express.Router();
 
 router.route("/signup").post(authController.createUser);
 router.route("/login").post(authController.loginUser);
 router.route("/logout").get(authController.logoutUser);
-router.route("/dashboard").get(authController.getDashboardPage);
+router.route("/dashboard").get(authware, authController.getDashboardPage);
 
 module.exports = router;
