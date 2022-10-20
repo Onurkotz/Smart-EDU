c0cca752c6383677c3c3bb86e660ff79f6ce62ca
onur koc <onurkottz@gmail.com>
2022-10-20 13:37:43 +0300

    nodemailer

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+// const nodemailer = require("nodemailer"); For sending mail.
+
 exports.getHome = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render("index", {
@@ -22,3 +24,55 @@ exports.getLoginPage = (req, res) => {
     page_name: "login",
   });
 };
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render("contact", {
+    page_name: "contact",
+  });
+};
+
+/* exports.sendMail = async (req, res) => {
+
+  const outputMessage = `
+  
+  <h1>Mail Details </h1>
+  <h2>From: </h2>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h2>Message</h2>
+  <p>${req.body.message}</p>
+  `
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "onurkottz@gmail.com", // gmail account
+      pass: "ARVR191919", // gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart Edu Portal Contact Form" <onurkottz@gmail.com>', // sender address
+    to: "ounurkouc@gmail.com", // list of receivers
+    subject: "Smart Edu Portal New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('contact');
+
+};
+
+For sending mail. But after May 2022 Google does not allow to send mail from 3rd party applications. Instead of this you can use Amazon SES.
+ */
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -8,5 +8,7 @@ router.route("/").get(pageController.getHome);
 router.route("/about").get(pageController.getAbout);
 router.route("/register").get(redirectware, pageController.getRegisterPage);
 router.route("/login").get(redirectware, pageController.getLoginPage);
+router.route("/contact").get(pageController.getContactPage);
+// router.route('/contact').post(pageController.sendMail); For sending mail. But after May 2022 Google does not allow to send mail from 3rd party applications.
 
 module.exports = router;
